3f82f68c466f585ee86618a1e9c26fc79995a638
cygday <amethystpun98@gmail.com>
2025-06-27 17:13:46 +0545

    app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/hello', (req, res) => {
+	const name = req.body.name;
+	res.send(`<h1> hello, ${name}! </h1>`);
+});
+	app.listen(port, () => {
+		console.log('app running at http://localhost:${port}');
+	});
+
+
